87ece10b86a4aeb7b8cd55d2acc546076ac62fd0
Mayank Kumar <mayank.kumar@cuddll.com>
2018-05-15 18:28:16 +0530

    dotenv setup

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
 
 const app = express();
-var uri = 'mongodb+srv://mayank:mayank25@cluster0-wgedf.mongodb.net/cuddll?retryWrites=true'
+var uri = process.env.DB_URI
 mongoose.connect(uri)
 mongoose.connection.once('open', () => {
     console.log('conneted to database');
